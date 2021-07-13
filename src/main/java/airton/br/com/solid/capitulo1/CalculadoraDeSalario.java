c8dc6d132d0c3bed5b0f8cb1f4c060cfd8bce3ec
Airton Oliveira <airtonoliveira@users.noreply.github.com>
2021-07-13 20:29:06 -0300

    Cap. 01 - Single Responsibility Principle

---
+++ src/main/java/airton/br/com/solid/capitulo1/CalculadoraDeSalario.java
@@ -0,0 +1,9 @@
+package airton.br.com.solid.capitulo1;
+
+public class CalculadoraDeSalario {
+
+	public double calcula(Funcionario funcionario){
+		return funcionario.calcularSalario();
+	}
+
+}
---
+++ src/main/java/airton/br/com/solid/capitulo1/RegraDeCalculo.java
@@ -0,0 +1,5 @@
+package airton.br.com.solid.capitulo1;
+
+public interface RegraDeCalculo {
+	double calcula(Funcionario funcionario);
+}
